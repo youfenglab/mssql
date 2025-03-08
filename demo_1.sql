1127ba5f682d0d1b81866a3490ebc96ba21ba5bc
Youfeng <zhouyoufeng@gmail.com>
2025-03-08 23:27:27 +1000

    add empty Lines

--- demo_1.sql
+++ demo_1.sql
@@ -4,4 +4,6 @@ SET @Name = N'Engineering'
 SET @sqlstring = N'SELECT * FROM AdventureWorks2022.HumanResources.Department WHERE Name = @Name'
 EXEC sp_executesql @sqlstring, N'@Name NVARCHAR(50)', @Name = @Name
 
+
+
 SELECT GETDATE() AS Today;
